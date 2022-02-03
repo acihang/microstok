f50bf6464e415a670ea2f89ae26b58470f9c7437
acihang <cihanaligngr@gmail.com>
2022-02-03 06:21:06 +0300

    Ürün microservisi oluşturuldu.

---
+++ microstok-product/src/test/java/com/cihanalidev/microstokproduct/MicrostokProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cihanalidev.microstokproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrostokProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
